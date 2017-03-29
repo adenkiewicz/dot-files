62983c6ff04954dda65fa089220e9cd8228f4df0
Adrian Denkiewicz <adrian.denkiewicz@gmail.com>
2017-03-29 13:19:04 +0200

    Fixed install.sh

--- install.sh
+++ install.sh
@@ -4,10 +4,10 @@ DOT_FILES_DIR="$(dirname $(readlink -f $0))"
 
 # vim
 ln -s $DOT_FILES_DIR/vimrc ~/.vimrc
-ln -s $DOT_FILES_DIR/vim ~/.vim
+if [ ! -d ~/.vim ]; then ln -s $DOT_FILES_DIR/vim ~/.vim; fi
 
 # tmux
 ln -s $DOT_FILES_DIR/tmux.conf ~/.tmux.conf
 
 # zsh
-ln -s $DOT_FILES_DIR/zsh/.zshrc ~/.zshrc
+ln -s $DOT_FILES_DIR/zsh/zshrc ~/.zshrc
