5d322a95d2425b9f5f1fbdbb574af17d3ea4c4f9
Adrian Denkiewicz <adrian.denkiewicz@gmail.com>
2017-03-31 10:14:19 +0200

    zsh: added no_proxy helper

--- zsh/helpers.zsh
+++ zsh/helpers.zsh
@@ -19,3 +19,14 @@ function extract {
       echo "'$1' is not a valid file"
   fi
 }
+
+function no_proxy {
+    unset HTTP_PROXY
+    unset HTTPS_PROXY
+    unset FTP_PROXY
+    unset ALL_PROXY
+    unset http_proxy
+    unset https_proxy
+    unset ftp_proxy
+    unset all_proxy
+}
