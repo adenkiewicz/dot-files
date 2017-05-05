1233e80e7ee741c4473682898e369fef104090be
Adrian Denkiewicz <adrian.denkiewicz@gmail.com>
2017-05-05 09:42:59 +0200

    zsh: replacing GREP_OPTIONS with simple alias

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,5 +1,6 @@
 alias ls='ls --color -Fh'
 alias ll='ls -l'
 alias ctags='ctags -R --fields=+l'
+alias grep='grep --color -n ' # apparently GREP_OPTIONS is deprecated
 
 alias sudo='nocorrect sudo ' # prevent disasters when running under root
--- zsh/exports.zsh
+++ zsh/exports.zsh
@@ -8,7 +8,6 @@ export EDITOR="vim"
 export VISUAL="vim"
 export PAGER='less'
 export TERM=rxvt
-export GREP_OPTIONS='--color -n'
 
 # Keep 1000 lines of history within the shell and save it to ~/.zsh_history:
 export HISTSIZE=1000
