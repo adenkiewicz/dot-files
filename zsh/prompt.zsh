eeddd32933fabc7077ec7165f52c411a96c42cd0
Adrian Denkiewicz <adenkiewicz@users.noreply.github.com>
2017-11-20 21:55:13 +0100

    zsh: PS1 - show full hostname

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,4 +1,4 @@
 # Set up the prompt
 autoload -Uz promptinit && promptinit
 
-export PS1="$(print '%{\e[31m%}<%{\e[0m%}%n%{\e[34m%}@%{\e[0m%}%m%{\e[31m%}>%{\e[0m%}%~%(?.%{\e[0m%}.%{\e[32m%})%(!.#.$)%{\e[0m%} ')"
+export PS1="$(print '%{\e[31m%}<%{\e[0m%}%n%{\e[34m%}@%{\e[0m%}%M%{\e[31m%}>%{\e[0m%}%~%(?.%{\e[0m%}.%{\e[32m%})%(!.#.$)%{\e[0m%} ')"
