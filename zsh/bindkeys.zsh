d3730e129dece823f6ff35f33d8566096353641e
Adrian Denkiewicz <adenkiewicz@users.noreply.github.com>
2018-02-16 21:01:16 +0100

    zsh: no more Emacs bindings, long live Vim

--- zsh/bindkeys.zsh
+++ zsh/bindkeys.zsh
@@ -1,8 +1,23 @@
-# Use emacs keybindings even if our EDITOR is set to vi
-bindkey -e
+# Always use vim bindings
+bindkey -v
 
-# fix for delete key
-bindkey "^[[3~"  delete-char
-bindkey "^[3;5~" delete-char
-bindkey "^[[1~"  beginning-of-line
-bindkey "^[[4~"  end-of-line
+# change prompt when in VIM mode
+function zle-line-init zle-keymap-select {
+    VIM_PROMPT=$(print '%{\033[3;31m%}<% VIM>% %{\e[0m%}')
+    RPS1="${${KEYMAP/vicmd/$VIM_PROMPT}/(main|viins)/}"
+    RPS2=$RPS1
+    zle reset-prompt
+}
+
+zle -N zle-line-init
+zle -N zle-keymap-select
+
+# use RPS1 only on current line
+setopt transientrprompt
+
+# remove delay before entering in VIM mode
+export KEYTIMEOUT=1
+
+# fix backspace behavior after coming from VIM mode
+bindkey '^?' backward-delete-char
+bindkey '^h' backward-delete-char
